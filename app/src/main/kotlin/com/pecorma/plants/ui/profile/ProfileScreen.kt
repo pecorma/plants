448b3440e206b13497f9dd372e178c796bf33fe7
jpecoraro181 <jpecoraro181@alivecor.com>
2022-05-05 16:49:26 -0400

    refactor location of clear ripple theme for general use

--- app/src/main/kotlin/com/pecorma/plants/ui/profile/ProfileScreen.kt
+++ app/src/main/kotlin/com/pecorma/plants/ui/profile/ProfileScreen.kt
@@ -5,21 +5,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.TabRow
 import androidx.compose.material.TabRowDefaults
 import androidx.compose.material.ripple.LocalRippleTheme
-import androidx.compose.material.ripple.RippleAlpha
-import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
@@ -44,6 +40,7 @@ import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.PagerState
 import com.google.accompanist.pager.pagerTabIndicatorOffset
 import com.google.accompanist.pager.rememberPagerState
+import com.pecorma.plants.ui.theme.utils.ClearRippleTheme
 import kotlinx.coroutines.launch
 
 @Composable
@@ -54,7 +51,7 @@ fun ProfileScreen() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .windowInsetsPadding(WindowInsets.systemBars)
+                .systemBarsPadding()
                 .background(color = MaterialTheme.colorScheme.surface),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -104,18 +101,29 @@ fun ProfileScreen() {
                     pagerState
                 )
             }
-            HorizontalPager(
+            ProfileHorizontalPager(
                 modifier = Modifier.fillMaxSize(),
-                verticalAlignment = Alignment.CenterVertically,
-                count = 3,
-                state = pagerState
-            ) {
-                when (pagerState.currentPage) {
-                    0 -> Text(text = "Layout 1")
-                    1 -> Text(text = "Layout 2")
-                    2 -> Text(text = "Layout 3")
-                }
-            }
+                pagerState = pagerState
+            )
+        }
+    }
+}
+
+@Composable
+fun ProfileHorizontalPager(
+    modifier: Modifier = Modifier,
+    pagerState: PagerState
+) {
+    HorizontalPager(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        count = 3,
+        state = pagerState
+    ) {
+        when (currentPage) {
+            0 -> Text(text = "Layout 1")
+            1 -> Text(text = "Layout 2")
+            2 -> Text(text = "Layout 3")
         }
     }
 }
@@ -184,19 +192,6 @@ fun ProfileTab(
             )
         }
     }
-}
-
-object ClearRippleTheme : RippleTheme {
-    @Composable
-    override fun defaultColor(): Color = Color.Transparent
-
-    @Composable
-    override fun rippleAlpha() = RippleAlpha(
-        draggedAlpha = 0.0f,
-        focusedAlpha = 0.0f,
-        hoveredAlpha = 0.0f,
-        pressedAlpha = 0.0f,
-    )
 }
 
 @Preview
